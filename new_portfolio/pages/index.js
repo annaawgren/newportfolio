2f731b72871bff7bf9bfa421954215766c739ab2
Anna Ågren <info@annaagren.se>
2020-11-27 11:03:34 +0100

    tailwind working

--- new_portfolio/pages/index.js
+++ new_portfolio/pages/index.js
@@ -1,13 +1,21 @@
+import React from "react";
 import Head from "next/head";
 import Link from "next/link";
 
 export default function Home() {
   return (
+    <React.Fragment>
+
+<Head>
+<title>Anna Wikberg Ågren Art Director</title>
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+<link rel="manifest" href="/site.webmanifest" />
+</Head>
+   
     <div className="container">
-      <Head>
-        <title>Anna Wikberg Ågren Art Director</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    
 
       <main>
         <h1 className="title">
@@ -17,29 +25,10 @@ export default function Home() {
           </Link>
         </h1>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <div className="bg-red-400 w-full h-screen text-white text-2xl text-center content-center">Hejsan</div>
 
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
+        
           <a
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className="card"
@@ -209,5 +198,8 @@ export default function Home() {
         }
       `}</style>
     </div>
+
+    </React.Fragment>
   );
 }
+
