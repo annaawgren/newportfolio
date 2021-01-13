48a4a2f5a9c34e6d92ac6894ae456f6dfd2b4d45
Anna Ågren <info@annaagren.se>
2021-01-13 12:43:58 +0100

    header change

--- new_portfolio/components/Header.js
+++ new_portfolio/components/Header.js
@@ -3,12 +3,15 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="w-full pt-8 pb-12 mb-12 sticky top-0 z-10 px-5 md:px-10 lg:px-20 ">
+    <header className="w-full flex flex-row justify-between pt-8 pb-12 mb-12 sticky top-0 z-10 px-5 md:px-10 lg:px-20 ">
       <Link href="/">
-        <a className="flex flex-col justify-top text-center">
-          <p className="soehne headerfont">
-            Anna Wikberg Agren <br /> Art Director \ Designer{" "}
-          </p>
+        <a className="">
+          <p className="soehne headerfont">Anna Wikberg Agren</p>
+        </a>
+      </Link>
+      <Link href="/">
+        <a className="">
+          <p className="soehne headerfont">Art Director \ Designer</p>
         </a>
       </Link>
     </header>
