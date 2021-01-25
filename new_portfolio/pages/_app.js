09c8be164920f2a30da70d23ff37b289245c3fcc
Pär Thernström <par.thernstrom@gmail.com>
2021-01-25 11:04:07 +0100

    Scroll to top of new page when following a link, instead of doing a smooth scroll up

--- new_portfolio/pages/_app.js
+++ new_portfolio/pages/_app.js
@@ -1,8 +1,30 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import { AnimateSharedLayout } from "framer-motion";
 import "../index.css";
 
+/**
+ * Scroll to top of new page when following a link instead of starting
+ * at same scroll position as prev page and then smoothly scrolling up.
+ *
+ * Solution from next.js issue #20125:
+ * https://github.com/vercel/next.js/issues/20125#issuecomment-757547865
+ */
+function useNormalScrollRoutes() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.events.on("routeChangeStart", () => {
+      document.documentElement.classList.add("normal-scroll");
+    });
+    router.events.on("routeChangeComplete", () => {
+      document.documentElement.classList.remove("normal-scroll");
+    });
+  }, []);
+}
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
+  useNormalScrollRoutes();
   return (
     <AnimateSharedLayout>
       <Component {...pageProps} />
